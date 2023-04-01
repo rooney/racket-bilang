08bb7b7ce08b3bd5a3adadbb9b7b9984ee77fafe
Ron Panduwana <panduwana@gmail.com>
2023-04-01 20:50:06 +0700

    Parsing rules cleanup

--- parser.rkt
+++ parser.rkt
@@ -2,20 +2,20 @@
 
 expres : /feeds? expr4
 @expr4 : expr3 /(SPACE|feeds)?
-@expr3 : apply3|macro3|combo
-       | exprZ
-@exprZ : combo0|combo1
-       | macro0|macro1|macroL
+@expr3 : apply3|macro3|split
        | expr2
-@expr2 : apply2|comma
-       | exprK
-@exprK : comma1|comma0
+@expr2 : apply2|macro2
+       | macroL|macro1|macro0
+       | exprL
+@exprL : split1|split0
+       | exprk
+@exprk : comma1|comma0|comma
        | expr1
 @expr1 : apply1
        | exprQ
-@exprQ : k0
-       | key /COLON
-       | spread
+@exprQ : key /COLON
+       | kv
+       | xtend
        | expr0
 @expr0 : apply0
        | bottom
@@ -23,65 +23,68 @@ expres : /feeds? expr4
        | exprG
 @exprG : applyG
        | grouping
-       | prop
+       | dotx
        | this
        | e
 
 @m1    : (comma|comma0|comma1|expr1) /SPACE
-@mL    : (combo|combo0|combo1)       /SPACE
-@mR    :                             /SPACE (exprK|macro1)
+@mL    : (split|split0|split1)       /SPACE
+@mR    :                             /SPACE (exprk|macro1|macro0)
 macro0 :          op
 macro1 :          op mR
        |  m1      op mR?
 macroL :     mL   op mR?
-macro2 : (m1|mL)? op mR? dent
-macro3 : (m1|mL)? op mR? dent? /feeds expr3
+macro2 : (m1|mL)? op mR? indent
+macro3 : (m1|mL)? op mR? indent? /feeds expr3
 
 @comma : (comma0|comma1|expr1) /SPACE? /COMMA
-@combo : (combo0|combo1)       /SPACE? /COMMA
-       |                exprZ  /NEWLINE /COMMA
+@split : (split0|split1)       /SPACE? /COMMA
+       |                expr2  /NEWLINE /COMMA
 
 comma1 : (comma|comma0) /SPACE expr1
-combo1 : (combo|combo0) /SPACE expr1
-comma0 :  comma         (prop|op|grouping)+
-combo0 :  combo         (prop|op|grouping)+
+split1 : (split|split0) /SPACE expr1
 
-apply3 : expr2 /feeds expr3
-apply2 : (expr1|comma0|combo0|comma|combo) dent
-apply1 : exprQ /SPACE expr1
+comma0 : comma (dotx|op|grouping)+
+split0 : split (dotx|op|grouping)+
+
+apply3 : (exprL|apply2) /feeds expr3
+apply2 :  exprL indent
+apply1 :  exprQ /SPACE expr1
 apply0 : (applyG|grouping) e
-       | (exprG|op) (prop|op)+
+       | (exprG|op) (dotx|op)+
 applyG : (expr0|op) grouping+
-apply  : (times|id|op) (int|dec|id|times+)
-parse  : (times|id|op|apply) string
+opid   : (opnum|op) id
+parse  : (op|e) string
+opnum  : op (int|dec)
 times  : (int|dec) id
-@e     : times|parse|apply|string|int|dec|id
+@e     : int|dec|id|string
+       | times|opid|opnum|parse
 
 op     : OP
 id     : ID
 idx    : ID (/DOT ID)*
 int    : INTEGER
 dec    : DECIMAL
-k0     : key /COLON expr0
 key    : idx | (OP|ID|INTEGER|DECIMAL)+
+kv     : @key COLON expr0
 atom   : (COLON @key?)? COLON @idx?
-prop   : /DOT (OP|OP? @id|grouping)
+dotx   : /DOT (OP|OP? @id|grouping)
 this   : /THIS
 feeds  : /(NEWLINE|pseudent)+
 string : /QUOTE /INDENT (STRING|interp|NEWLINE)* /DEDENT /UNQUOTE
        | /QUOTE         (STRING|interp)*                 /UNQUOTE
-interp : INTERPOLATE (brace|dent)
+interp : INTERPOLATE (brace|indent)
 
 @grouping : @paren|bracket|brace|generator
 paren     : /LPAREN grouped? /RPAREN
 bracket   : /LBRACK grouped? /RBRACK
 brace     : /LBRACE grouped? /RBRACE
 bottom    : /LBRACE /SPREAD  /RBRACE
-generator : /LBRACE /SPREAD /SPACE expr4      /RBRACE
-          | /LBRACE /SPREAD (id|@dent) /RBRACE
+generator : /LBRACE /SPREAD /SPACE expr4 /RBRACE
+          | /LBRACE /SPREAD (id|@indent) /RBRACE
 @grouped  : /SPACE? expr4 /SPACE?
-          | @dent /feeds
+          | @indent /feeds
           | OP
-spread    : /SPREAD (id|grouped)
-dent      : /INDENT expres    /DEDENT
+xtend     : /SPREAD (id|grouped)
+indent    : /INDENT expres /DEDENT
 pseudent  : /INDENT pseudent? /DEDENT
