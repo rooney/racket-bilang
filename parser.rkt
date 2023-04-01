802319b11b0bdcc3899a9f87e3a9b9b82908bd36
Ron Panduwana <panduwana@gmail.com>
2023-04-02 04:46:19 +0700

    Simplify rules of spaceless-applies for consistency

--- parser.rkt
+++ parser.rkt
@@ -16,15 +16,17 @@ expres : /feeds? expr4
 @exprQ : key /COLON
        | kv
        | xtend
-       | expr0
-@expr0 : apply0
+       | exprC
+@exprC : applyC
        | bottom
-       | atom
+       | symbol
        | exprG
 @exprG : applyG
        | grouping
-       | dotx
+       | expr0
+@expr0 : apply0
        | this
+       | dot
        | e
 
 @m1    : (comma|comma0|comma1|expr1) /SPACE
@@ -44,21 +46,19 @@ macro3 : (m1|mL)? op mR? indent? /feeds expr3
 comma1 : (comma|comma0) /SPACE expr1
 split1 : (split|split0) /SPACE expr1
 
-comma0 : comma (dotx|op|grouping)+
-split0 : split (dotx|op|grouping)+
+comma0 : comma (e|(dot|op|grouping)+)
+split0 : split (e|(dot|op|grouping)+)
 
 apply3 : (exprL|apply2) /feeds expr3
 apply2 :  exprL indent
 apply1 :  exprQ /SPACE expr1
-apply0 : (applyG|grouping) e
-       | (exprG|op) (dotx|op)+
-applyG : (expr0|op) grouping+
-opid   : (opnum|op) id
-parse  : (op|e) string
-opnum  : op (int|dec)
-times  : (int|dec) id
-@e     : int|dec|id|string
-       | times|opid|opnum|parse
+applyC : (applyG|grouping) e
+       | (exprG|op) (dot|op)+
+applyG : (exprC|op) grouping+
+apply0 : (expr0|op) e
+@e     : id
+       | int|dec
+       | string
 
 op     : OP
 id     : ID
@@ -66,9 +66,9 @@ idx    : ID (/DOT ID)*
 int    : INTEGER
 dec    : DECIMAL
 key    : idx | (OP|ID|INTEGER|DECIMAL)+
-kv     : @key COLON expr0
-atom   : (COLON @key?)? COLON @idx?
-dotx   : /DOT (OP|OP? @id|grouping)
+kv     : key /COLON exprC
+symbol : (COLON key?)? COLON idx?
+dot    : /DOT (OP|OP? @id|grouping)
 this   : /THIS
 feeds  : /(NEWLINE|pseudent)+
 string : /QUOTE /INDENT (STRING|interp|NEWLINE)* /DEDENT /UNQUOTE
