e4a9e88c3dd4e3b8456ab7dd7caa65ac02c7bc65
Ron Panduwana <panduwana@gmail.com>
2023-04-07 06:39:10 +0700

    Parser cleanup

--- parser.rkt
+++ parser.rkt
@@ -2,20 +2,20 @@
 
 expres : /feeds? expr4
 @expr4 : expr3 /(SPACE|feeds)?
-@expr3 : apply3|macro3|split
+@expr3 : apply3|macro3|cline
        | expr2
 @expr2 : apply2|macro2
        | macroL|macro1|macro0
        | exprL
-@exprL : split1|split0
+@exprL : cline1|cline0
        | exprk
 @exprk : comma1|comma0|comma
        | expr1
 @expr1 : apply1
        | exprQ
-@exprQ : kv|key /COLON
+@exprQ : keyval|key /COLON
+       | prop|field /COLON
        | xtend
-       | prop|field
        | exprO
 @exprO : applyO
        | atom
@@ -30,7 +30,7 @@ expres : /feeds? expr4
 
 macro  : DOTDOT|OP
 @m1    : (comma|comma0|comma1|expr1) /SPACE
-@mL    : (split|split0|split1)       /SPACE
+@mL    : (cline|cline0|cline1)       /SPACE
 @mR    :                             /SPACE (exprk|macro1|macro0)
 macro0 :          macro
 macro1 :          macro mR
@@ -39,15 +39,15 @@ macroL :  mL      macro mR?
 macro2 : (mL|m1)? macro mR? indent
 macro3 : (mL|m1)? macro mR? indent? /feeds expr3
 
-@comma : (comma0|comma1|expr1) /SPACE? /COMMA
-@split : (split0|split1)       /SPACE? /COMMA
+@comma : (comma0|comma1|expr1) /SPACE?  /COMMA
+@cline : (cline0|cline1)       /SPACE?  /COMMA
        |                expr2  /NEWLINE /COMMA
 
 comma1 : (comma|comma0) /SPACE expr1
-split1 : (split|split0) /SPACE expr1
+cline1 : (cline|cline0) /SPACE expr1
 
 comma0 : comma (dot|op|grouping)+
-split0 : split (dot|op|grouping)+
+cline0 : cline (dot|op|grouping)+
 
 apply3 : (exprL|apply2) /feeds expr3
 apply2 :  exprL indent
@@ -65,18 +65,18 @@ idx    : ID (/DOT ID)*
 int    : INTEGER
 dec    : DECIMAL
 key    : idx | (OP|ID|INTEGER|DECIMAL)+
-kv     : key /COLON exprO
-atom   : (COLON key?)? COLON idx?
+pubkey :         /COLON key?
+keyval : @key    /COLON exprO
+prop   : @field  /COLON exprO
+atom   : pubkey? /COLON idx?
+field  : /DOT (OP|OP? ID)
 dot    : /DOT (OP|OP? ID|grouping)
-field  : /DOT (OP|ID) /COLON
-prop   : @field exprO
 this   : /THIS
-feeds  : /(NEWLINE|pseudent)+
 string : /QUOTE /INDENT (STRING|interp|NEWLINE)* /DEDENT /UNQUOTE
        | /QUOTE         (STRING|interp)*                 /UNQUOTE
 interp : INTERPOLATE (brace|indent)
 
-@grouping : @paren|bracket|brace|generator
+@grouping : paren|bracket|brace|generator
 paren     : /LPAREN grouped? /RPAREN
 bracket   : /LBRACK grouped? /RBRACK
 brace     : /LBRACE grouped? /RBRACE
@@ -89,3 +89,4 @@ generator : /LBRACE /DOTDOT /SPACE expr4 /RBRACE
 xtend     : /DOTDOT (id|grouping)
 indent    : /INDENT expres /DEDENT
 pseudent  : /INDENT pseudent? /DEDENT
+feeds     : /(NEWLINE|pseudent)+
