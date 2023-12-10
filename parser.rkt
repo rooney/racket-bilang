ab8b3bc366b65b1e6f8da9fa0fd00ed85084ab7b
Ron Panduwana <panduwana@gmail.com>
2023-12-10 20:30:06 +0700

    Remove support for dot-OPERATOR (for example: x.+)

--- parser.rkt
+++ parser.rkt
@@ -33,7 +33,7 @@ expres : /feeds? expr4
        | LBRACE RBRACE
        | DOT COLON
        | def COLON
-       | bublet
+       | BUBLET
        | op
 macroZ :     macro kwargs
        | mL  macro kwargs?
@@ -45,16 +45,16 @@ comma  : (commaO|comma1|apply1) /SPACE?  /COMMA
 break  : (breakO|break1)        /SPACE?  /COMMA
        |                exprB   /NEWLINE /COMMA
 break1 : (@break|breakO) kwargs? /SPACE (kv |expr1)
-break2 : (@break|breakO) kwargs? /SPACE (kvZ|apply2)
+break2 : (@break|breakO) kwargs? /SPACE (kv2|apply2)
 comma1 : (@comma|commaO) kwargs? /SPACE (kv |expr1)
-comma2 : (@comma|commaO) kwargs? /SPACE (kvZ|apply2)
+comma2 : (@comma|commaO) kwargs? /SPACE (kv2|apply2)
 
 breakO : @break (bracket|dot|slash|op)+
 commaO : @comma (bracket|dot|slash|op)+
 
 apply3 : (expr2|break2|break1|breakO|break|macro|macroZ) /feeds expr3
 apply2 :  exprO /SPACE apply2
-       |  exprO kwargs? (/SPACE kvZ|indent)
+       |  exprO kwargs? (/SPACE kv2|indent)
 apply1 :  exprO kwargs? /SPACE (kv|expr1)
 applyO :  atom   bracket+
 apply0 :  expr0 (bracket|dot|slash|op)+
@@ -70,16 +70,16 @@ int    : INTEGER
 dec    : DECIMAL
 id     : (DOLLAR|DASH)? IDENTIFIER
 op     : OPERATOR|DOLLAR|DASH|SLASH
-bublet : BUBLET
 key    :              @op|@id
 atom   :      /COLON (@op|IDENTIFIER|COLON)?
 prop   : @def /COLON exprO
 kv     : @key /COLON exprO
-kvZ    : @key /COLON (/SPACE exprZ|@indent)
-def    : (@self|@self? @dot+) @op?
-dot    : /DOT @key
-self   : /DOT /SLASH DOLLAR? IDENTIFIER
-slash  : /SLASH IDENTIFIER
+kv2    : @key /COLON (/SPACE exprZ|@indent)
+def    : self op? (/DOT @key op?)*
+       |          (/DOT @key op?)+
+dot    :           /DOT @id
+self   :           /DOT /SLASH DOLLAR? IDENTIFIER
+slash  :                /SLASH         IDENTIFIER
 it     : /IT
 @str   : /QUOTE /INDENT (STRING|interp|NEWLINE)* /DEDENT /UNQUOTE
        | /QUOTE         (STRING|interp)*                 /UNQUOTE
