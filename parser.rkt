1adaa8b9c7b4f7839f4b7a66dfcf3a06b1372d17
Ron Panduwana <panduwana@gmail.com>
2024-01-04 10:18:53 +0700

    - New syntax for comma-separated array - Refactor, rename, code cleanup

--- parser.rkt
+++ parser.rkt
@@ -7,7 +7,7 @@ expres : /feeds? expr4
 @exprB : break2|break1|breakO|break
        | expr2
 @expr2 : apply2
-       | explo2
+       | expl2
        | comma2|comma1|commaO|comma
        | expr1
 @expr1 : apply1
@@ -21,81 +21,81 @@ expres : /feeds? expr4
        | e
 @exprl : expr1|comma1|commaO|comma
 @exprL : exprl|break1|breakO|break
-@exprI : exprl|commaI|exploI|applyI
+@exprI : exprl|commaI|applyI|explI
 
-exploI :                 radical kwargs
-       |  exprl /SPACE   radical kwargs?
-       | (exprl /SPACE)? radical kwargs? (spaceI|/SPACE atom)
-       | (exprl /SPACE)? radical kwargs? 
-explo2 :                 radical kwargs
-       |  exprL /SPACE   radical kwargs?
-       | (exprL /SPACE)? radical kwargs? (space2|/SPACE (atom|expr2))
+explI  :                 radic kwargs
+       |  exprl /SPACE   radic kwargs?
+       | (exprl /SPACE)? radic kwargs? (spaceI|/SPACE atom)
+       | (exprl /SPACE)? radic kwargs? 
+expl2  :                 radic kwargs
+       |  exprL /SPACE   radic kwargs?
+       | (exprL /SPACE)? radic kwargs? (space2|/SPACE (atom|expr2))
 
 comma  : (commaO|comma1|expr1) /SPACE? /COMMA
-commas : (commaO|comma1|expr1) /SPACE  /COMMA
+comma_ : (commaO|comma1|expr1) /SPACE  /COMMA
 break  : (breakO|break1)       /SPACE? /COMMA | exprB /NEWLINE /COMMA
-breaks : (breakO|break1)       /SPACE  /COMMA | exprB /NEWLINE /COMMA
-breakO :  @break (group|dot|slash|radical)+
-commaO :  @comma (group|dot|slash|radical)+
-comma2 : (@comma|commaO) kwargs? space2 | @commas  kv2
-commaI : (@comma|commaO) kwargs? spaceI | @commas  kvI
-comma1 : (@comma|commaO) kwargs? space1 | @commas (kv|exprO)
-break1 : (@break|breakO) kwargs? space1 | @breaks (kv|exprO)
-break2 : (@break|breakO) kwargs? space2 | @breaks  kv2 
+break_ : (breakO|break1)       /SPACE  /COMMA | exprB /NEWLINE /COMMA
+breakO :  @break (group|dot|slash|radic)+
+commaO :  @comma (group|dot|slash|radic)+
+comma2 : (@comma|commaO) kwargs? space2 | @comma_  kv2
+commaI : (@comma|commaO) kwargs? spaceI | @comma_  kvI
+comma1 : (@comma|commaO) kwargs? space1 | @comma_ (kv|exprO)
+break1 : (@break|breakO) kwargs? space1 | @break_ (kv|exprO)
+break2 : (@break|breakO) kwargs? space2 | @break_  kv2 
        |                              exprB /feeds kv2
-apply3 : (exprB|radical) (/SPACE key /COLON)? /feeds expr3
+apply3 : (exprB|radic) (/SPACE key /COLON)? /feeds expr3
 apply2 :  exprO kwargs? space2
 apply1 :  exprO kwargs? space1
 applyI :  exprO kwargs? spaceI
 applyO :  anion (id|string|group) group*
        |    ion e group*
-apply0 :  expr0 (dot|slash|radical|group)+
+apply0 :  expr0 (dot|slash|radic|group)+
        |  exprO string
-       |  radical e
+       |  radic e
        |  (int|dec) id
 @e     :   int|dec|id
        |   string
        |   group
 
-int     :  INTEGER
-dec     :  DECIMAL
-radical :  atom
-        |  DOT @key? COLON
-        |  LPAREN RPAREN
-        |  LBRACE RBRACE
-        | (DOLLAR|DASH|SLASH|RADICAL|QUESTION-MARK) PRIME?
-nucleon : (DOLLAR|DASH)? IDENTIFIER (QUESTION-MARK|DASH)? PRIME?
-id      :  IDENTIFIER PRIME?
-ion     :      /COLON (COLON|@key)
-anion   :      /COLON
-kv      : @key /COLON        exprO
-kvI     : @key /COLON /SPACE exprI
-kv2     : @key /COLON /SPACE expr2
-        | @key /COLON @indent
-key     :  @radical | @id
-slash   :      /SLASH @id
-dot     :        /DOT @id
-it      : /IT
-str     : /QUOTE /INDENT (STRING|interpt|NEWLINE)* /DEDENT /UNQUOTE
-        | /QUOTE         (STRING|interpt)*                 /UNQUOTE
-backstr : /BACKTICK @str
-string  : @backstr | @str
-interpt : INTERPOLATE (braces|indent)
+int    :  INTEGER
+dec    :  DECIMAL
+radic  :  atom
+       |  DOT @key? COLON
+       |  LPAREN RPAREN
+       |  LBRACE RBRACE
+       | (DOLLAR|DASH|SLASH|RADICAL|QUESTION-MARK) PRIME?
+nuclei : (DOLLAR|DASH)? IDENTIFIER (QUESTION-MARK|DASH)? PRIME?
+id     :  IDENTIFIER PRIME?
+ion    :      /COLON (COLON|@key)
+anion  :      /COLON
+kv     : @key /COLON        exprO
+kvI    : @key /COLON /SPACE exprI
+kv2    : @key /COLON /SPACE expr2
+       | @key /COLON @indent
+key    :  @radic | @id
+slash  :    /SLASH @id
+dot    :      /DOT @id
+it     : /IT
+str    : /QUOTE /INDENT (STRING|interp|NEWLINE)* /DEDENT /UNQUOTE
+       | /QUOTE         (STRING|interp)*                 /UNQUOTE
+bacstr : /BACKTICK @str
+string : @bacstr | @str
+interp : INTERPOLATE (braces|indent)
 
-atom     : /LPAREN /PROTON ((@nucleon? COLON? /SPACE COLON)? @nucleon)? /RPAREN
-         | /LPAREN /PROTON             COLON                            /RPAREN
-parens   : /LPAREN  (subexpr|radical)         /RPAREN
-braces   : /LBRACE   subexpr                  /RBRACE
-brackets : /LBRACKET array?                   /RBRACKET
+atom     : /LPAREN /PROTON ((@nuclei? COLON? /SPACE COLON)? @nuclei)? /RPAREN
+         | /LPAREN /PROTON            COLON                           /RPAREN
+parens   : /LPAREN  (subexpr|radic) /RPAREN
+braces   : /LBRACE   subexpr        /RBRACE
+brackets : /LBRACKET array?         /RBRACKET
 @group   :  parens | braces | brackets
 @subexpr :  /SPACE? expr4                | indent /feeds
 @space2  :  /SPACE          (kv2|apply2) | indent
 @spaceI  :  /SPACE (expr1|kv|kvI|applyI)
 @space1  :  /SPACE (expr1|kv)
 kwargs   : (/SPACE        kv)+
-@array   :  /SPACE  expr1 (/COMMA /SPACE expr1)* (/NEWLINE /SPACE? (/COMMA /SPACE expr1)+)* /SPACE
-         |          exprO        (/SPACE exprO)* (/NEWLINE                (/SPACE exprO)+)*
-         | /INDENT        /COMMA (/SPACE exprI|@indent)  (/NEWLINE /COMMA (/SPACE exprI|@indent))* /DEDENT /NEWLINE
+@array   :  /SPACE  exprI                        (/NEWLINE /SPACE? /COMMA  /SPACE exprI)*   /SPACE
+         |          exprO (/SPACE exprO)*        (/NEWLINE                (/SPACE exprO)+)*
+         | /INDENT /COMMA (/SPACE exprI|@indent) (/NEWLINE /COMMA (@indent|/SPACE exprI)*   /DEDENT /NEWLINE
 indent   : /INDENT @expres   /DEDENT
 pseudent : /INDENT pseudent? /DEDENT
 feeds    : /(NEWLINE|pseudent)+
